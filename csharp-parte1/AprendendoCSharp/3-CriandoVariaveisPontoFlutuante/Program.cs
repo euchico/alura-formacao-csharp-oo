cd3fea727765631ebb12a9fbe23b194baa604233
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-05-24 23:36:00 -0300

    Aula 3

---
+++ csharp-parte1/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
