f4d67e5025ee67d70c22c9d231ed5c477c7e09f1
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-05-24 23:52:54 -0300

    Aula 3

--- csharp-parte1/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
+++ csharp-parte1/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -14,8 +14,23 @@ namespace _3_CriandoVariaveisPontoFlutuante
 
             double salario;
             salario = 1200.70;
-
             Console.WriteLine(salario);
+
+            double idade;
+            idade = 15 / 2.0;
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0  / 3 = " + idade);
+
+            // Exercício
+            double peso = 4.0;
+            int quantidade = 10;
+            Console.WriteLine(peso * quantidade);
+
 
             Console.WriteLine("A execução acabou. Tecle enter para sair...");
             Console.ReadLine();
