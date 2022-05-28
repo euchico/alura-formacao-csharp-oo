f741aef0a6062160c1dee27007157505e0ca704e
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-05-27 23:51:24 -0300

    Aula 5

---
+++ csharp-parte1/AprendendoCSharp/10-CalculaPoupanca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 10 -  Calcula Poupança");
+
+            double valorInvestido = 1000;
+            int contatorMes = 1;
+
+            while (contatorMes <= 12)
+            {
+                // 0.36% = 0.0036
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contatorMes + " você terá R$" + valorInvestido);
+
+                // contadorMes = contatorMes + 1
+                // contadorMes += 1;
+                contatorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp-parte1/AprendendoCSharp/P11-CalculaPoupaca2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupaca2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 11");
+
+            double valorInvestido = 1000;
+
+            for (int contatorMes = 1; contatorMes <= 12; contatorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contatorMes + " você terá R$" + valorInvestido);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
