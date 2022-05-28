e2916f4fed5fc4a543d4828ae8bf9631e4d24111
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-05-28 00:15:43 -0300

    Inicio aula 6

---
+++ csharp-parte1/AprendendoCSharp/P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 11");
+
+            Console.ReadLine();
+        }
+    }
+}
