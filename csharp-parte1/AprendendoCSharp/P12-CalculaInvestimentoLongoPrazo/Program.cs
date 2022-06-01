456e00b6dd232152dcc7c7f69199feb973836dc1
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-05-31 22:48:11 -0300

    Aula 6

--- csharp-parte1/AprendendoCSharp/P12-CalculaInvestimentoLongoPrazo/Program.cs
+++ csharp-parte1/AprendendoCSharp/P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -10,7 +10,21 @@ namespace P12_CalculaInvestimentoLongoPrazo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando Projeto 11");
+            Console.WriteLine("Executando Projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 0; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
 
             Console.ReadLine();
         }
---
+++ csharp-parte1/AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            // Desenhando com Break
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= 10; contadorColuna++)
+                {
+                    Console.Write("*");
+
+                    if (contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // Uma forma diferente de fazer o desenho de asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
