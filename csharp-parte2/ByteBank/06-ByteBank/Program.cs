41f602ea04ab339a63a38b91bb851536956ec02a
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-06-20 22:17:53 -0300

    Parte 2 Aula 5

---
+++ csharp-parte2/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "435.654.987-20";
+            cliente.Profissao = "Médico";
+
+            conta.Saldo = -10;
+
+            Console.WriteLine(conta.Titular);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
