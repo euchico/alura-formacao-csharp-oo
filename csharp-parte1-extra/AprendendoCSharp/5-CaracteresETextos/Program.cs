18c83e28933a5ab94f3a71c115051a682ddb7d73
Francisco de Paula <franciscodepaula.web@gmail.com>
2022-07-07 21:48:58 -0300

    Extra Aula 4

---
+++ csharp-parte1-extra/AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura - Curso de tecnologia ";
+        Console.WriteLine(primeiraFrase + 2022);
+
+        string vazia = "";
+        Console.WriteLine(vazia);
+
+        string cursos = @"Cursos disponíveis: 
+- Go
+- C#
+- Python
+- Java";
+        Console.WriteLine(cursos);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
